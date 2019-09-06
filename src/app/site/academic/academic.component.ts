d27502d9b7b2fdf5d42b15b448d0a29078ab42f7
Samuel Nygaard <teamnygaard@gmail.com>
2019-09-06 20:28:56 +0200

    materializecss

--- src/app/site/academic/academic.component.ts
+++ src/app/site/academic/academic.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./academic.component.scss']
 })
 export class AcademicComponent implements OnInit {
-
+  elem = 'This is a test';
+  content = [this.elem];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addElem() {
+    this.content.push(this.elem);
+    this.content.push(this.elem);
+    this.content.push(this.elem);
+    this.content.push(this.elem);
+  }
+
+  clear() {
+    this.content = [];
   }
 
 }
