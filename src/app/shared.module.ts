0dcf2a811d11cce8fd57cac7fff3fa8ad0eabac4
Samuel Nygaard <teamnygaard@gmail.com>
2019-09-06 18:17:42 +0200

    full structure

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const MATERIAL_MODULES: any[] = [MatToolbarModule];
 
 @NgModule({
-  exports: [CommonModule, TranslateModule]
+  imports: [MATERIAL_MODULES],
+  exports: [CommonModule, TranslateModule, MATERIAL_MODULES]
 })
 export class SharedModule {}
--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -3,16 +3,50 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SiteComponent } from './site.component';
 import { SharedModule } from '../shared.module';
+import { HomeComponent } from './home/home.component';
+import { AcademicComponent } from './academic/academic.component';
+import { EnterpriseComponent } from './enterprise/enterprise.component';
+import { HealthcareComponent } from './healthcare/healthcare.component';
+import { ResearchComponent } from './research/research.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: SiteComponent
+    component: SiteComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'academic',
+        component: AcademicComponent
+      },
+      {
+        path: 'enterprise',
+        component: EnterpriseComponent
+      },
+      {
+        path: 'healthcare',
+        component: HealthcareComponent
+      },
+      {
+        path: 'research',
+        component: ResearchComponent
+      }
+    ]
   }
 ];
 
 @NgModule({
-  declarations: [SiteComponent],
+  declarations: [
+    SiteComponent,
+    HomeComponent,
+    AcademicComponent,
+    EnterpriseComponent,
+    HealthcareComponent,
+    ResearchComponent
+  ],
   imports: [CommonModule, RouterModule.forRoot(routes), SharedModule],
   exports: [RouterModule]
 })
